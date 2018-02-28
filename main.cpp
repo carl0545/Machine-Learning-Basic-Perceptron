613c8ccd773db59179467057e9f30ea388621e23
carl0545 <carl0545@vandals.uidaho.edu>
2018-02-27 18:02:06 -0800

    Fills matrix with values from stdin

--- main.cpp
+++ main.cpp
@@ -10,10 +10,11 @@
 
 
 
-using namespace std; //Function reads in total inputs, rows and column for the matrix from stdin
+using namespace std;
 
-void setBasics(int*, int*, int*);
-void setValuesf(Matrix*);
+void setBasics(int*, int*, int*); //Function reads in total inputs, rows and column for the matrix from stdin
+void setValuesf(Matrix*); //Function sets the elements of the matrix from stdin
+void printMatrix(Matrix); //Prints the contents matrix
 
 int main(){
   int u_inputs, u_rows, u_cols;
@@ -23,13 +24,7 @@ int main(){
 
   train = new Matrix(u_rows, u_cols, "train");
 
-  
-
-
-
-
-
-
+  setValuesf(&train);
 
 
 }
@@ -43,7 +38,34 @@ void setBasics(int *input, int *rows, int *cols){
   cin >> *cols;
 }
 
+/*
+*Function sets the elements of the matrix from stdin
+*/
 void setValuesf(Matrix *m){
 
+  for(int r = 0; r < m->numRows(); r++){
+    for(int c = 0; c < m->numCols(); c++){
+      int element;
+      cin >> element;
+      m->set(r,c, element);
+    }
+  }
+
+
+}
+
+/*
+*Prints the contents matrix
+*/
+void printMatrix(Matrix m){
+  int rows = m.numRows();
+  int cols = m.numCols();
+
+  for(int r = 0; r < rows; r++){
+    for(int c = 0; c < cols; c++){
+      cout << m.get(r,c) << " ";
+    }
+    cout << endl;
+  }
 
 }
